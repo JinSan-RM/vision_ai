404878b7f109786df94d7d2c100da2db23dc1f68
JinSan-RM <yug6789@naver.com>
2024-05-13 11:57:42 +0900

    [KJS] 색칠공부

--- Coloring/main.py
+++ Coloring/main.py
@@ -1,3 +1,23 @@
+    # =============================
+    #
+    # *Test Desription
+    #
+    # =============================
+
 from fastapi import FastAPI
+from PIL import Image
+import tensorflow as tf
+import numpy as np
+from io import BytesIO
+import os, requests, cv2
+from ImgCrtl import ImgCtrl
 
 app = FastAPI()
+
+@app.get('/')
+def mainDef( source : str):
+    ImgPixcel = ImgCtrl.imageUrlToPixels( source )
+    
+    return 
+
+
