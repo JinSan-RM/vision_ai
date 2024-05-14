c3065dce2532619629a4536e1f403753a25fb564
JinSan-RM <yug6789@naver.com>
2024-05-14 14:18:54 +0900

    [귐진산 feat 이서혁] main.py re upload

--- Coloring/Boundary/Boundary.py
+++ Coloring/Boundary/Boundary.py
@@ -1,7 +1,18 @@
+    #   =================================================
+    #
+    #   이미지내부의 AI 결과값을 토대로 바운더리 박스 스케치
+    #   
+    #   스케치된 Boundary Box 
+    #
+    #   =================================================
+    
 import cv2
 import numpy as np
 
 def ImgBoundarySketch(img, output_data):
+    img_Width, img_Height = img.shape[:2]
+    print(img_Width, img_Height, "<=========shape")
+    cv2.rectangle(img, (0, 0), (1920, 1080), (255, 255, 255), thickness = cv2.FILLED)
     if img is None:
         print("로딩 실패")
     for detection in output_data:
