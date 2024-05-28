963e0f061ccf649c67be9624fdb550a54ee76953
JinSan-RM <yug6789@naver.com>
2024-05-28 18:01:56 +0900

    [orb test]

--- similarity_analyzer/Similarity/ssim_similarity.py
+++ similarity_analyzer/Similarity/ssim_similarity.py
@@ -105,7 +105,7 @@ def feature_matching(img1, img2):
     img1 = resize_with_aspect_ratio(img1, target_size)
     img2 = resize_with_aspect_ratio(img2, target_size)
     # ORB 파라미터 조정
-    orb = cv2.ORB_create(nfeatures=500, scaleFactor=1.2, nlevels=8, edgeThreshold=0, firstLevel=0, WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE, patchSize=5, fastThreshold=20)
+    orb = cv2.ORB_create(nfeatures=500, scaleFactor=1, nlevels=8, edgeThreshold=0, firstLevel=0, WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE, patchSize=40, fastThreshold=30)
     
     kp1, des1 = orb.detectAndCompute(img1, None)
     kp2, des2 = orb.detectAndCompute(img2, None)
@@ -124,18 +124,18 @@ def feature_matching(img1, img2):
     FLANN_INDEX_LSH = 6
     
     index_params= dict(algorithm = FLANN_INDEX_LSH,
-                    table_number = 6,
-                    key_size = 12,
-                    multi_probe_level = 1)
+                    table_number = 15,
+                    key_size = 20,
+                    multi_probe_level = 5)
     
-    search_params=dict(checks=32)
+    search_params=dict(checks=40)
     matcher = cv2.FlannBasedMatcher(index_params, search_params)
     matches = matcher.match(des1, des2)
     # 매칭 그리기
-    # res = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, \
-    #             flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    res = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, \
+                flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     
-    # cv2.imwrite('/code/Img/res1.jpg', res)
+    cv2.imwrite('/code/Img/res1.jpg', res)
     # 디버깅 정보 출력
     print(f'Number of keypoints in image 1: {len(kp1) if kp1 is not None else 0}')
     print(f'Number of keypoints in image 2: {len(kp2) if kp2 is not None else 0}')
@@ -155,81 +155,6 @@ def feature_matching(img1, img2):
     cv2.imwrite('/code/Img/img_matches.jpg', img_matches)
     return img_matches, len(matches)
 
-
-# def extract_color_mask(img, color):
-#     # Define range for the color (example for blue color)
-#     lower_bound = np.array([color[0] - 10, color[1] - 10, color[2] - 10])
-#     upper_bound = np.array([color[0] + 10, color[1] + 10, color[2] + 10])
-#     mask = cv2.inRange(img, lower_bound, upper_bound)
-#     return mask
-
-# def feature_matching(img1, img2, color1, color2, weight1=0.9, weight2=0.1):
-#     # ORB 파라미터 조정
-#     orb = cv2.ORB_create(nfeatures=500, scaleFactor=1.2, nlevels=8, edgeThreshold=31, firstLevel=0, WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE, patchSize=31, fastThreshold=20)
-
-#     # Extract masks for the given colors
-#     mask1_img1 = extract_color_mask(img1, color1)
-#     mask2_img1 = extract_color_mask(img1, color2)
-#     mask1_img2 = extract_color_mask(img2, color1)
-#     mask2_img2 = extract_color_mask(img2, color2)
-
-#     # Extract keypoints and descriptors for each color mask
-#     kp1_color1, des1_color1 = orb.detectAndCompute(img1, mask1_img1)
-#     kp1_color2, des1_color2 = orb.detectAndCompute(img1, mask2_img1)
-#     kp2_color1, des2_color1 = orb.detectAndCompute(img2, mask1_img2)
-#     kp2_color2, des2_color2 = orb.detectAndCompute(img2, mask2_img2)
-
-#     # Ensure descriptors are not None and are continuous
-#     if des1_color1 is not None:
-#         des1_color1 = np.ascontiguousarray(des1_color1)
-#     if des1_color2 is not None:
-#         des1_color2 = np.ascontiguousarray(des1_color2)
-#     if des2_color1 is not None:
-#         des2_color1 = np.ascontiguousarray(des2_color1)
-#     if des2_color2 is not None:
-#         des2_color2 = np.ascontiguousarray(des2_color2)
-
-#     # Initialize matcher
-#     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-#     # Match descriptors for each color and calculate weighted matches
-#     matches_color1 = []
-#     if des1_color1 is not None and des2_color1 is not None:
-#         matches_color1 = bf.match(des1_color1, des2_color1)
-#         matches_color1 = sorted(matches_color1, key=lambda x: x.distance)
-
-#     matches_color2 = []
-#     if des1_color2 is not None and des2_color2 is not None:
-#         matches_color2 = bf.match(des1_color2, des2_color2)
-#         matches_color2 = sorted(matches_color2, key=lambda x: x.distance)
-
-#     # Calculate the number of matches for each color with weights
-#     num_matches_color1 = len(matches_color1)
-#     num_matches_color2 = len(matches_color2)
-#     weighted_num_matches = weight1 * num_matches_color1 + weight2 * num_matches_color2
-
-#     # Draw matches for debugging
-#     img_matches_color1 = cv2.drawMatches(img1, kp1_color1, img2, kp2_color1, matches_color1, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-#     img_matches_color2 = cv2.drawMatches(img1, kp1_color2, img2, kp2_color2, matches_color2, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-#     cv2.imwrite('/code/Img/res1_color1.jpg', img_matches_color1)
-#     cv2.imwrite('/code/Img/res2_color2.jpg', img_matches_color2)
-
-#     return weighted_num_matches
-
-# def calculate_combined_similarity(img1, img2, color1, color2, weight1=0.95, weight2=0.05, weight_ssim=0., weight_orb=0.5):
-#     ssim_similarity = ssim_similarity_calculator(img1, img2)
-
-#     orb_similarity = feature_matching(img1, img2, color1, color2, weight1, weight2)
-
-#     keypoints_img_1 = cv2.ORB_create().detect(img1, None)
-#     keypoints_img_2 = cv2.ORB_create().detect(img2, None)
-#     max_possible_matches = min(len(keypoints_img_1), len(keypoints_img_2))
-#     orb_similarity_normalized = orb_similarity / max_possible_matches if max_possible_matches > 0 else 0
-
-#     combined_similarity = (weight_ssim * ssim_similarity) + (weight_orb * orb_similarity_normalized)
-#     print(f'Combined Similarity: {combined_similarity}')
-
-#     return combined_similarity
 
 def feature_matching_with_shi_tomasi(img1, img2):
     list1 = []
